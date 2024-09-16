d3746d754c9f0555f36f22a3a9176dd2968e8596
YuanWang08 <sckwilson1008@gmail.com>
2024-09-17 01:17:27 +0800

    output

---
+++ frontend/dist/assets/AboutView-BPp9ZQKs.js
@@ -0,0 +1,1 @@
+import{_ as o,o as s,a as t,b as a}from"./index-Cq_WT0L-.js";const n={},r={class:"about"};function c(_,e){return s(),t("div",r,e[0]||(e[0]=[a("h1",null,"This is an about page",-1)]))}const l=o(n,[["render",c]]);export{l as default};
